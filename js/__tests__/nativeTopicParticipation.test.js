28dc8f0c4dee524c161d18159814a7f0993980fe
Alex Alvarez <aalvarez3@gmail.com>
2026-08-24 00:29:52 -0400

    test(native): bind participation scope profile

--- js/__tests__/nativeTopicParticipation.test.js
+++ js/__tests__/nativeTopicParticipation.test.js
@@ -20,9 +20,19 @@ describe('native topic participation', () => {
     expect(source).toContain(
       'reply_to_post_number: composer.replyToPostNumber',
     );
-    expect(siteManagerSource).toContain(
-      "'read,write,notifications,session_info,one_time_password,adjuster-network-renaissance:member_discovery'",
-    );
+    for (const scope of [
+      "'read'",
+      "'write'",
+      "'notifications'",
+      "'session_info'",
+      "'one_time_password'",
+      "'adjuster-network-renaissance:member_discovery'",
+      "'adjuster-network-renaissance:profile_onboarding'",
+      "'adjuster-network-renaissance:creator_delete'",
+      'ADMISSION_HANDOFF_SCOPE',
+    ]) {
+      expect(siteManagerSource).toContain(scope);
+    }
   });
 
   test('provides an accessible native composer and refreshes after posting', () => {
